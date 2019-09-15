7991a370e4faf6b6721bfe4e32a3fe43b69d7a9d
Fawzi <alami42@live.com>
2019-09-15 08:54:09 -0500

    initial folder structure and sql files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('American Cheese Burger',false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon And cheese',false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Cheese Burger',false);
